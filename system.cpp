4599979fa10034c9983f28e8769c1869892995b0
Tim Nibert <docz2a@gmail.com>
2018-06-23 22:21:30 +1000

    off we go!

---
+++ system.cpp
@@ -0,0 +1,29 @@
+#include <stdint.h>
+
+// 6502 condition codes?
+typedef struct ConditionCodes {
+
+} ConditionCodes;
+
+typedef struct cpustate {
+    uint8_t a;
+    uint8_t x;
+    uint8_t y;
+    uint8_t p;
+    uint8_t sp;
+    uint16_t pc;
+} cpustate;
+
+class Famicom {
+    cpustate * cpu;
+}
+
+// create an abstract class for instructions
+class opcode
+{
+    unsigned char * opcode;
+    char * opcodestr;
+    virtual void action()=0;
+}
+
+// create a hash table for instructions
