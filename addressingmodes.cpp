d4457d4bae8c79f6184b59479425c968799768d8
Tim Nibert <docz2a@gmail.com>
2019-10-20 16:53:21 +1100

    updated addressing modes for new interface, program counter increments

--- addressingmodes.cpp
+++ addressingmodes.cpp
@@ -10,31 +10,21 @@
  * We assume that the pc is pointing at the current opcode
  * return final data (uint8_t)
  *
- * This is the process of being refactored to match the above
+ * todo: double check these, unit test
  *
  * todo: update cycle count for every memory access
  */
 
- // immediate addressing is passing the value specified in code
+// immediate addressing is passing the value specified in code
 uint8_t immediate(cpustate * cpu, Memory * mem)
  {
      cpu->pc++;
      return mem->readmem(cpu->pc);
  }
 
-uint8_t indexedindirect(cpustate * cpu, Memory * mem)
-{
-    // todo: what to increment pc by?
-    // (Indirect,X) aka (zero page,X)
-    uint8_t zpageaddr = mem->readmem(cpu->pc+1);
-    uint16_t addr = zpageaddr + cpu->x;
-    // we have to read from addr to find the new addr to load from?
-    uint16_t finaladdr = revlendianbytes(mem->readmem(addr), mem->readmem(addr+1));
-    return mem->readmem(finaladdr);
-}
-
 uint8_t zeropage(cpustate * cpu, Memory * mem)
 {
+    cpu->pc++;
     uint8_t finaladdr = mem->readmem(cpu->pc+1);
     return mem->readmem(finaladdr);
 }
@@ -49,22 +39,33 @@ uint8_t absolute(cpustate * cpu, Memory * mem)
 uint8_t zeropagex(cpustate * cpu, Memory * mem)
 {
     cpu->pc++;
-    return mem->readmem(cpu->pc+1)+cpu->x;
+    return mem->readmem(mem->readmem(cpu->pc+1)+cpu->x);
 }
 
 uint8_t zeropagey(cpustate * cpu, Memory * mem)
 {
     cpu->pc++;
-    return mem->readmem(cpu->pc+1)+cpu->y;
+    return mem->readmem(mem->readmem(cpu->pc+1)+cpu->y);
+}
+
+uint8_t indexedindirect(cpustate * cpu, Memory * mem)
+{
+    // (Indirect,X) aka (zero page,X)
+    cpu->pc++;
+    uint8_t zpageaddr = mem->readmem(cpu->pc+1);
+    uint16_t addr = zpageaddr + cpu->x;
+    // we have to read from addr to find the new addr to load from?
+    uint16_t finaladdr = revlendianbytes(mem->readmem(addr), mem->readmem(addr+1));
+    return mem->readmem(finaladdr);
 }
 
 uint8_t indirectindexed(cpustate * cpu, Memory * mem)
 {
-    // todo: what to increment pc by?
     // (Indirect),Y aka (zero page),Y
+    cpu->pc++;
     uint8_t zpageaddr = mem->readmem(cpu->pc+1);
     uint16_t addr = (revlendianbytes(mem->readmem(zpageaddr), mem->readmem(zpageaddr+1))) + cpu->y;
-    return addr;
+    return mem->readmem(addr);
 }
 
 uint8_t absolutey(cpustate * cpu, Memory * mem)
@@ -72,7 +73,7 @@ uint8_t absolutey(cpustate * cpu, Memory * mem)
     cpu->pc+=2;
     uint16_t addr = revlendianbytes(mem->readmem(cpu->pc+1), mem->readmem(cpu->pc+2));
     addr += cpu->y;
-    return addr;
+    return mem->readmem(addr);
 }
 
 uint8_t absolutex(cpustate * cpu, Memory * mem)
@@ -80,5 +81,5 @@ uint8_t absolutex(cpustate * cpu, Memory * mem)
     cpu->pc+=2;
     uint16_t addr = revlendianbytes(mem->readmem(cpu->pc+1), mem->readmem(cpu->pc+2));
     addr += cpu->x;
-    return addr;
+    return mem->readmem(addr);
 }
